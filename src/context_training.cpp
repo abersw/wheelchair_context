61fb224d59dff893cdfcd7a16e7626ad710bff39
Tomos Fearn <tfearn10@outlook.com>
2020-02-12 12:42:49 +0000

    commented out unused libraries

--- src/context_training.cpp
+++ src/context_training.cpp
@@ -14,9 +14,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <iostream> //io library
+//#include <unistd.h>
+//#include <getopt.h>
+//#include <iostream> //io library
 #include <fstream> //for writing and reading files in c++
 #include <ros/package.h> //find ROS packages, needs roslib dependency
 
