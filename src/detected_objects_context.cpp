be200bc3c642ef3de585e168b7f7bc590349571c
Tomos Fearn <tof7@aber.ac.uk>
2021-03-22 23:13:20 +0000

    added libraries and subscriber topic

--- src/detected_objects_context.cpp
+++ src/detected_objects_context.cpp
@@ -5,10 +5,28 @@
  * Status: Pre-Alpha
 */
 
-int main (int argc, char **argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ros/ros.h" //main ROS library
+#include "ros/package.h" //find ROS packages, needs roslib dependency
+#include "wheelchair_msgs/objectLocations.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+void objectLocationsCallback(const wheelchair_msgs::objectLocations obLoc) {
+
+}
+
+int main (int argc, char **argv) {
     //add code here
     //notes:
     //take UID from publish_objects_location and pass it through here
     //when msg comes through with UID of object - append a room name to the object
+    ros::init(argc, argv, "detected_objects_context");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("wheelchair_robot/dacop/publish_object_locations/detected_objects", 10, objectLocationsCallback);
     return 0;
 }
