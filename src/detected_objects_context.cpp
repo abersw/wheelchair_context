4169e939065b46d32436469c27a8e03d66e9b760
Tomos Fearn <tof7@aber.ac.uk>
2021-03-22 14:08:33 +0000

    added extra file

---
+++ src/detected_objects_context.cpp
@@ -0,0 +1,11 @@
+/*
+ * detected_objects_context.cpp
+ * wheelchair_context
+ * version: 0.0.1 Majestic Maidenhair
+ * Status: Pre-Alpha
+*/
+
+int main (int argc, char **argv[]) {
+    //add code here
+    return 0;
+}
