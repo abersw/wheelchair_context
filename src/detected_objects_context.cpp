6dae1e661157f372897ea0a41c2e0ffccdd5dd2f
Tomos Fearn <tof7@aber.ac.uk>
2022-01-01 18:24:24 +0000

    added code for determining object history to function

--- src/detected_objects_context.cpp
+++ src/detected_objects_context.cpp
@@ -492,6 +492,69 @@ void contextNoHistory(int detPos) {
 }
 
 /**
+ * History exists, therefore compare with history to see if object was in previous frame
+ *
+ */
+void contextWithHistory() {
+    for (int detectedObject = 0; detectedObject < totalObjectsDetectedStruct[0]; detectedObject++) { //run through struct of detected objects
+        int getDetObjID = objectsDetectedStruct[0][detectedObject].id; //get id
+        std::string getDetObjName = objectsDetectedStruct[0][detectedObject].object_name; //get name
+
+        for (int lastDetectedObject = 0; lastDetectedObject < totalObjectsDetectedStruct[1]; lastDetectedObject++) { //run through struct of last detected objects
+            int getLastObjID = objectsDetectedStruct[1][detectedObject].id; //get id
+            std::string getLastObjName = objectsDetectedStruct[1][detectedObject].object_name; //get name
+
+            if ((getDetObjID == getLastObjID) && (getDetObjName == getLastObjName)) { //if object id and name match, it can still see the same object
+                //if match found, do not recalculate weighting - because it must be the same object in the frame
+            }
+            else {
+                //run through entire context struct for returning correct object id position
+                for (int isContext = 0; isContext < totalObjectContextStruct; isContext++) {
+                    int getContextID = objectContext[isContext].object_id; //get context ID
+                    std::string getContextName = objectContext[isContext].object_name; //get context name
+
+                    if ((getDetObjID == getContextID) && (getDetObjName == getContextName)) { //if object ID and name are equal
+                        tofToolBox->printSeparator(0);
+                        //if new object in detected struct pos 0 is equal to object in context
+                        //update object weighting and detected
+                        objectContext[isContext].object_detected++; //add one to times object was detected in env
+                        double isCurrentWeighting = objectContext[isContext].object_weighting;
+                        double isNewWeighting = isCurrentWeighting + trainingInfo.times_trained_val;
+                        if (DEBUG_detectedObjectCallback) {
+                            cout << "new weighting is " << isNewWeighting << endl;
+                        }
+                        if (isNewWeighting > trainingInfo.max_weighting) { //if outside of max weighting
+                            objectContext[isContext].object_weighting = trainingInfo.max_weighting; //assign object weighting max weight
+                            if (DEBUG_detectedObjectCallback) {
+                                cout << "set weighting to max: " << trainingInfo.max_weighting << endl;
+                            }
+                        }
+                        else if (isNewWeighting < trainingInfo.min_weighting) { //if outside of min weighting
+                            objectContext[isContext].object_weighting = trainingInfo.min_weighting; //assign object weighting min weight
+                            if (DEBUG_detectedObjectCallback) {
+                                cout << "set weighting to min: " << trainingInfo.min_weighting << endl;
+                            }
+                        }
+                        else { //if inside bounding weight
+                            objectContext[isContext].object_weighting = isNewWeighting; //assign object weighting caluclated weight
+                            if (DEBUG_detectedObjectCallback) {
+                                cout << "assigned to context struct pos " << isContext << " weighting " << objectContext[isContext].object_weighting << endl;
+                            }
+                        }
+                        //work out uniqueness
+                    }
+                    else {
+                        //skip over, don't assign anything if detected object and context don't match
+                    }
+                }
+            }
+
+        }
+    }
+    shiftObjectsDetectedStructPos(0,1); //shift detection data from struct pos 0 to 1
+}
+
+/**
  * start calculating weighting value for training session
  *
  */
@@ -547,58 +610,7 @@ void detectedObjectCallback(const wheelchair_msgs::objectLocations obLoc) {
         if (DEBUG_detectedObjectCallback) {
             cout << "data exists in struct pos " << detPos+1 << endl;
         }
-        for (int detectedObject = 0; detectedObject < totalObjectsDetectedStruct[0]; detectedObject++) { //run through struct of detected objects
-            int getDetObjID = objectsDetectedStruct[0][detectedObject].id; //get id
-            std::string getDetObjName = objectsDetectedStruct[0][detectedObject].object_name; //get name
-            for (int lastDetectedObject = 0; lastDetectedObject < totalObjectsDetectedStruct[1]; lastDetectedObject++) { //run through struct of last detected objects
-                int getLastObjID = objectsDetectedStruct[1][detectedObject].id; //get id
-                std::string getLastObjName = objectsDetectedStruct[1][detectedObject].object_name; //get name
-                if ((getDetObjID == getLastObjID) && (getDetObjName == getLastObjName)) { //if object id and name match, it can still see the same object
-                    //if match found, do not recalculate weighting - because it must be the same object in the frame
-                }
-                else {
-                    //run through entire context struct for returning correct object id position
-                    for (int isContext = 0; isContext < totalObjectContextStruct; isContext++) {
-                        int getContextID = objectContext[isContext].object_id; //get context ID
-                        std::string getContextName = objectContext[isContext].object_name; //get context name
-                        if ((getDetObjID == getContextID) && (getDetObjName == getContextName)) { //if object ID and name are equal
-                            tofToolBox->printSeparator(0);
-                            //if new object in detected struct pos 0 is equal to object in context
-                            //update object weighting and detected
-                            objectContext[isContext].object_detected++; //add one to times object was detected in env
-                            double isCurrentWeighting = objectContext[isContext].object_weighting;
-                            double isNewWeighting = isCurrentWeighting + trainingInfo.times_trained_val;
-                            if (DEBUG_detectedObjectCallback) {
-                                cout << "new weighting is " << isNewWeighting << endl;
-                            }
-                            if (isNewWeighting > trainingInfo.max_weighting) { //if outside of max weighting
-                                objectContext[isContext].object_weighting = trainingInfo.max_weighting; //assign object weighting max weight
-                                if (DEBUG_detectedObjectCallback) {
-                                    cout << "set weighting to max: " << trainingInfo.max_weighting << endl;
-                                }
-                            }
-                            else if (isNewWeighting < trainingInfo.min_weighting) { //if outside of min weighting
-                                objectContext[isContext].object_weighting = trainingInfo.min_weighting; //assign object weighting min weight
-                                if (DEBUG_detectedObjectCallback) {
-                                    cout << "set weighting to min: " << trainingInfo.min_weighting << endl;
-                                }
-                            }
-                            else { //if inside bounding weight
-                                objectContext[isContext].object_weighting = isNewWeighting; //assign object weighting caluclated weight
-                                if (DEBUG_detectedObjectCallback) {
-                                    cout << "assigned to context struct pos " << isContext << " weighting " << objectContext[isContext].object_weighting << endl;
-                                }
-                            }
-                            //work out uniqueness
-                        }
-                        else {
-                            //skip over, don't assign anything if detected object and context don't match
-                        }
-                    }
-                }
-            }
-        }
-        shiftObjectsDetectedStructPos(0,1); //shift detection data from struct pos 0 to 1
+        contextWithHistory();
     }
     //calculate uniqueness here would probably work - doesn't need to detect an object to calculate - probably quicker too...
 }
